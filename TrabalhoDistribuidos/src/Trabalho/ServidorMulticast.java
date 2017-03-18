8d0cd32bc6e6ee2203ef513fbd68fbb30f3283a6
Jonathan <jonathan.michels7@gmail.com>
2017-03-18 17:21:32 -0300

    Implementado tela e inicio de comunicação, verificar comentarios na classe servidormulticast..

--- TrabalhoDistribuidos/src/Trabalho/ServidorMulticast.java
+++ TrabalhoDistribuidos/src/Trabalho/ServidorMulticast.java
@@ -30,14 +30,25 @@ class ServidorMulticast {
 		
 			//Identifica a mensagem
 			if (sentence.toLowerCase().contains("backup")) {
+				//Testando conexão com servidor tcp
+				//esse servidor chama o servidor tcp passando as informações do cliente que  requisitou o backup
+				// assim o servidor tcp devera retornar para o cliente o datagram que seria o DatagramPacket 
+				// assim o cliente terá conhecimento do servidor tcp e conversara diretamente com ele
+				//Pelo que entendi esse servidor só servira para criar conexão com o servidor tcp
+				//Deve ser implementado para que passe a porta e o InetAddress para o servidor tcp
+				//e ele pegue essas informações para ter a comunicação com o cliente
+				//até então foi isso
+				Cliente cliente = new Cliente();
+				cliente.setNomeServidor(InetAddress.getByName("localhost"));
+				cliente.setNumeroPorta(9988);
+				cliente.setMensagemEnviar("HORAS");
+				cliente.ClienteTCP();
 				retorno = "teste efetuado".getBytes();
 			} else {
 				retorno = "Teste falhou".getBytes();
 			}
 			
 			pacoteEnvio = new DatagramPacket (retorno, retorno.length, pacoteRecebido.getAddress(), pacoteRecebido.getPort());
-			pacoteEnvio.setAddress(InetAddress.getByName("localhost"));
-			pacoteEnvio.setPort(9988);
 //			socket.setTimeToLive (ttl);
 			socket.send(pacoteEnvio);
 
